64acc5248cb09d9d53502e04590d2ec91d572dd9
rubayetseason <rubayetislamseason5@gmail.com>
2025-12-30 17:22:33 +0600

    initiated module for product

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductController } from './product/product.controller';
 import { ProductService } from './product/product.service';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
+  imports: [ProductModule],
   controllers: [AppController, ProductController],
   providers: [AppService, ProductService],
 })
